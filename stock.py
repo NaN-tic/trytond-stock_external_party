f8cdb498c35bcaff1afb230d501d2ba650a4f64d
Raimon Esteve <raimon@nan-tic.com>
2018-12-04 13:36:33 +0100

    FIX compute_quantities_query parameters

--- stock.py
+++ stock.py
@@ -155,8 +155,7 @@ class Move:
 
     @classmethod
     def compute_quantities_query(cls, location_ids, with_childs=False,
-            grouping=('product',), grouping_filter=None,
-            quantity_field='internal_quantity'):
+            grouping=('product',), grouping_filter=None):
         context = Transaction().context
 
         new_grouping = grouping[:]
