e1bb1c3699706d6f859bb7cfc2951c5a32599527
Bernat Brunet <bernat@nan-tic.com>
2021-04-26 14:57:41 +0200

    Revert "If stock_lot is installed, allow computing lot quantites based on party."
    This reverts commit 77f08d8d825a1ef183d0fdf29dc92380183b2a7b.

--- __init__.py
+++ __init__.py
@@ -24,7 +24,3 @@ def register():
     Pool.register(
         product.ProductByParty,
         module='stock_external_party', type_='wizard')
-    Pool.register(
-        stock.Lot,
-        depends=['stock_lot'],
-        module='stock_external_party', type_='model')
